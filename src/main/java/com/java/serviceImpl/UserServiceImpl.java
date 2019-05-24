1696d3e2074633003ff195402967966275077bb9
chenyy <chenyy@lihfinance.com>
2019-05-24 10:01:30 +0800

    新增spring注解测试

--- src/main/java/com/java/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/java/serviceImpl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.java.serviceImpl;
 
 import com.java.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service("userService")
 public class UserServiceImpl implements UserService {
     @Override
     public void addusers() {
--- src/main/java/com/java/test/UserTest.java
+++ src/main/java/com/java/test/UserTest.java
@@ -1,16 +1,29 @@
 package com.java.test;
 
 import com.java.service.UserService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
 public class UserTest {
+    @Autowired
+    private UserService userService;
     @Test
     public void test () {
         String xmlPath = "ApplicationContext.xml";
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
         UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.addusers();
+
+    }
+    @Test
+    public void test2 () {
         userService.addusers();
 
     }
